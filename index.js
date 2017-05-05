deb607bf904104d52244d2d1a02ab3386b6fefb1
JailBreak <admin@vgee.cn>
2017-05-05 15:08:11 +0800

    优化构造函数, 修复带#链接的解析错误，增加 13例单元测试

--- index.js
+++ index.js
@@ -6,123 +6,134 @@
  *
  * urlQuery.push({page: '{{number}}'}).toString(false) # "?page={{number}}"
  *
- * urlQuery.push({page: '{{number}}'}).toString()      # "?page=%7B%7Bnumber%7D%7D"
+ * urlQuery.push({page: '{{number}}'}).toString()   # "?page=%7B%7Bnumber%7D%7D"
  *
  * urlQuery.push({test: 'tt'})
  *
- * urlQuery.getObj()                                   # {page: "1", test: "tt"}
+ * urlQuery.getObj()                  # {page: "1", test: "tt"}
  *
- * urlQuery.getUrl();                                  # "http://example.com/?page=%7B%7Bnumber%7D%7D"
+ * urlQuery.getUrl();                 # "http://example.com/?page=%7B%7Bnumber%7D%7D"
  *
- * urlQuery.del('page').getUrl();                      # "http://example.com/?test=tt"
+ * urlQuery.del('page').getUrl();           # "http://example.com/?test=tt"
  *
  */
 
 
 var UrlQuery = function(url) {
-    this._url = url.split('?')[0];
-    this._query = {};
-    var qstr = url.split('?')[1] || '';
+  if(!(this instanceof UrlQuery)) {
+    return new UrlQuery(url)
+  }
 
-    if(!qstr) {
-    	return
-    }
+  if(!url) {
+    url = window.location.href
+  }
 
-    var a = qstr.split('&');
+  var querySplit = url.split('?');
+  this._url = querySplit[0];
+  this._query = {};
 
-    for (var i = 0; i < a.length; i++) {
-        var b = a[i].split('=');
-        this._query[decodeURIComponent(b[0])] = decodeURIComponent(b[1] || '');
-    }
+  var getQueryStr = function() {
+    var qstr = querySplit[1] ? querySplit[1].split('#')[0] : false
+    return qstr || ''
+  }
+
+  var getHashStr = function() {
+    var hstr = querySplit[1] ? querySplit[1].split('#')[1] : false
+    return hstr ? '#' + hstr : ''
+  }
+
+  var qstr = getQueryStr()
+  this._hash = getHashStr()
+
+  if (!qstr) {
+    return this
+  }
+
+  var a = qstr.split('&');
+
+  for (var i = 0; i < a.length; i++) {
+    var b = a[i].split('=');
+    this._query[decodeURIComponent(b[0])] = decodeURIComponent(b[1] || '');
+  }
 };
 
 // params: {'key': 'value'}
 // params: 'key', 'value'
 UrlQuery.prototype.push = function(obj) {
-	var that = this;
-    var args = Array.prototype.slice.call(arguments)
+  var that = this;
+  var args = Array.prototype.slice.call(arguments)
 
-    if(args.length == 2) {
-        that._query[args[0]] = args[1];
-        return this;
+  if (args.length == 2) {
+    that._query[args[0]] = args[1];
+    return this;
+  }
+  if (typeof obj == 'object') {
+    for (var key in obj) {
+      if (!obj.hasOwnProperty(key)) {
+        continue;
+      }
+      var key = key,
+        val = obj[key];
+      that._query[key] = val;
     }
-    if(typeof obj == 'object') {
-        for(var key in obj) {
-            if(!obj.hasOwnProperty(key)) {
-                continue;
-            }
-            var key = key,
-                val = obj[key];
-            that._query[key] = val;
-        }
-	   return this;
-    }
-    throw new Error('Unexpected params');
+    return this;
+  }
+  throw new Error('Unexpected params');
 };
 
 UrlQuery.prototype.get = function(key) {
 
-    return this._query[key];
+  return this._query[key];
 
 };
 
 UrlQuery.prototype.getObj = function() {
 
-    return this._query;
+  return this._query;
 
 };
 
 
 UrlQuery.prototype.getQuery = function(urlEncode) {
 
-	urlEncode = urlEncode === undefined ? true : urlEncode;
-    var result = [];
-    for(var query in this._query) {
-    	
-    	if(!this._query.hasOwnProperty(query)) {
-            continue;
-    	}
-    	var key = query,
-    		val = this._query[query];
+  urlEncode = urlEncode === undefined ? true : urlEncode;
+  var result = [];
+  for (var query in this._query) {
 
-    	if(urlEncode) {
-			result.push(encodeURIComponent(key) + '=' + encodeURIComponent(val));
-		} else {
-			result.push(key + '=' + val);
-		}
+    if (!this._query.hasOwnProperty(query)) {
+      continue;
     }
-    return ('?' + result.join("&"));
+    var key = query,
+      val = this._query[query];
+
+    if (urlEncode) {
+      result.push(encodeURIComponent(key) + '=' + encodeURIComponent(val));
+    } else {
+      result.push(key + '=' + val);
+    }
+  }
+  var ret = result.join("&")
+  return ret ? '?' + ret : ''
 
 };
 
 UrlQuery.prototype.getUrl = function(urlEncode) {
 
-    return this._url + this.getQuery(urlEncode);
+  return this._url + this.getQuery(urlEncode) + this._hash;
 
 };
 
 UrlQuery.prototype.del = function(key) {
 
-	delete this._query[key];
-	return this;
+  delete this._query[key];
+  return this;
 
 };
 
-UrlQuery.prototype.getStr = UrlQuery.prototype.toString = UrlQuery.prototype.query = UrlQuery.prototype.getQuery 
+UrlQuery.prototype.getStr = UrlQuery.prototype.toString = UrlQuery.prototype.query = UrlQuery.prototype.getQuery
 
 UrlQuery.prototype.getAll = UrlQuery.prototype.getObj;
 
 UrlQuery.prototype.url = UrlQuery.prototype.getUrl
 
-module.exports = function (url) {
-    if(url) {
-        return new UrlQuery(url);
-    }
-    if(typeof window == 'undefined') {
-        throw new Error('Can\'t not find window.location.href');
-    }
-    if(window && window.location && window.location.href) {
-        return new UrlQuery(window.location.href);
-    }
-    throw new Error('Unexpected Error');
-};
+module.exports = UrlQuery
--- test.js
+++ test.js
@@ -1,24 +1,73 @@
+var should = require('should');
 var UrlQuery = require('./index.js');
 
-var urlQuery = UrlQuery('https://www.insta360.com?foo=oof&bar=rab')
+describe('urlQuery', function () {
+  var urlQuery = UrlQuery('https://www.insta360.com?foo=oof&bar=rab')
 
-if (urlQuery.push({'someParam': 'someValue'}).getStr() != '?foo=oof&bar=rab&someParam=someValue') {
-  throw new Error('Test fail');
-}
-if (urlQuery.push('otherParam', 'otherValue').getStr() != '?foo=oof&bar=rab&someParam=someValue&otherParam=otherValue') {
-  throw new Error('Test fail');
-}
-if (urlQuery.del('foo').getStr() != '?bar=rab&someParam=someValue&otherParam=otherValue') {
-  throw new Error('Test fail');
-}
-if (urlQuery.get('bar') != 'rab') {
-  throw new Error('Test fail');
-}
-if (urlQuery.getUrl() != 'https://www.insta360.com?bar=rab&someParam=someValue&otherParam=otherValue') {
-  throw new Error('Test fail');
-}
-if(JSON.stringify(urlQuery.getObj()) != '{"bar":"rab","someParam":"someValue","otherParam":"otherValue"}') {
-  throw new Error('Test fail');
-}
+  it('should init whit params', function() {
+    urlQuery.getObj().should.have.properties({
+      foo: 'oof',
+      bar: 'rab'
+    })
+  })
 
-console.log('Test complete');
+  it('should push obj in params', function() {
+    urlQuery.push({'someParam': 'someValue'}).getObj().should.have.properties({
+      someParam: 'someValue',
+      foo: 'oof',
+      bar: 'rab'
+    })
+  })
+
+  it('should del obj in params', function() {
+    urlQuery.del('someParam').getObj().should.not.have.properties({
+      someParam: 'someValue'
+    })
+  })
+
+  it('should get a string params value', function() {
+    urlQuery.get('foo').should.equal('oof')
+  })
+
+  it('should get a string params value', function() {
+    urlQuery.get('foo').should.equal('oof')
+  })
+
+  it('should get a full url string', function() {
+    urlQuery.getUrl().should.equal('https://www.insta360.com?foo=oof&bar=rab')
+  })
+
+  it('should get a full url string', function() {
+    urlQuery.getUrl().should.equal('https://www.insta360.com?foo=oof&bar=rab')
+  })
+
+  it('should maintain hash value', function() {
+    var urlQuery = UrlQuery('https://www.insta360.com?foo=oof&bar=rab#hash')
+    urlQuery.getUrl().should.equal('https://www.insta360.com?foo=oof&bar=rab#hash')
+  })
+
+  it('should not have params', function() {
+    var urlQuery = UrlQuery('https://www.insta360.com#hash')
+    urlQuery.getUrl().should.equal('https://www.insta360.com#hash')
+  })
+
+  it('should not have params and hash', function() {
+    var urlQuery = UrlQuery('https://www.insta360.com')
+    urlQuery.getUrl().should.equal('https://www.insta360.com')
+  })
+
+  it('should not encode URL params', function() {
+    var urlQuery = UrlQuery('https://www.insta360.com')
+    urlQuery.push({page: '{{number}}'}).toString(false).should.equal('?page={{number}}')
+  })
+
+  it('should encode URL params', function() {
+    var urlQuery = UrlQuery('https://www.insta360.com')
+    urlQuery.push({page: '{{number}}'}).toString().should.equal('?page=%7B%7Bnumber%7D%7D')
+  })
+
+  it('should work with new or not', function() {
+    var urlQuery = new UrlQuery('https://www.insta360.com')
+    urlQuery.getUrl().should.equal('https://www.insta360.com')
+  })
+})
